31ff013faa1839c02885489b8cff612a0b6f429c
bitscoder <exhibit4real@gmail.com>
2024-06-18 18:54:06 +0100

    setting up render for managed postgres database

--- ai_blog_app/ai_blog_app/settings.py
+++ ai_blog_app/ai_blog_app/settings.py
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'ai_blog_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_db_jxm0',
+        'USER': 'blog_db_jxm0_user',
+        'PASSWORD': 'tWBVHo1lsYwC95iDV23ns8KjIaCLqFsY',
+        'HOST': 'dpg-cporejiju9rs738utai0-a.virginia-postgres.render.com',
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation
