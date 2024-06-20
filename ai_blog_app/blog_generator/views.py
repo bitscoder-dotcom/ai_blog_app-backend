5dbd5431331dcdc3478c1ab779366d1b4e076e2e
bitscoder <exhibit4real@gmail.com>
2024-06-20 08:17:28 +0100

    still working on the video download feature

--- ai_blog_app/blog_generator/views.py
+++ ai_blog_app/blog_generator/views.py
@@ -2,14 +2,89 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.conf import settings
+from pytube import YouTube
+import assemblyai as aai
+import json
+import os
+import openai
+
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 @login_required
 def index(request):
     return render(request, 'index.html')
 
+@csrf_exempt
 def generate_blog(request):
-    pass
+    if request == 'POST':
+        try:
+            data = json.loads(request.body)
+            yt_link = data['link']
+        except(KeyError, json.JSONDecodeError):
+            return JsonResponse({'error': 'Invalid data sent'}, status = 400)
+        
+    # get video title
+        title = yt_title(yt_link)
+
+    # get transcript
+        transcription = get_transcription(yt_link)
+        if not transcription:
+            return JsonResponse({'error': 'Failed to get transcript'}, status = 500)
+
+    # use openai to generate blog
+        blog_content = generate_blog_from_transcription(transcription)
+        if not blog_content:
+            return JsonResponse({'error': 'Failed to generate blog article'}, status = 500)
+
+    # save blog article to databse
+
+    # return blog article as a response 
+
+        return JsonResponse({'content': blog_content})
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status = 405)
+
+
+def yt_title(link):
+    yt = YouTube(link)
+    title = yt.title
+    return title
+
+def download_audio(link):
+    yt = YouTube(link)
+    video = yt.streams.filter(only_audio=True).first()
+    out_file = video.download(output_path=settings.MEDIA_URL)
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+    return new_file
+
+def get_transcription(link):
+    audio_file = download_audio(link)
+    aai.settings.api_key = '4185bb31750644a4aa09a56f6368d562'
+
+    transcriber = aai.Transcriber()
+    transcript = transcriber.transcribe(audio_file)
+
+    return transcript.text
+
+def generate_blog_from_transcription(transcript):
+    openai.api_key = 'sk-proj-DeC70sobbmntGvqvy9HWT3BlbkFJc0TTcGuwSsnoEs4Pr2fj'
+    prompt = f'''Based on the following transcript from a YouTube video, write a comprehensive blog article, 
+                write it based on the transcribe but do not make it look like a youtube video, make it look like
+                a proper blog article:\n\n{transcript}\n\nArticle:'''
+    response = openai.completions.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1000
+    )
+    generated_content = response.choices[0].text.strip
+
+    return generated_content
 
 def user_signup(request):
     if request.method == 'POST':
@@ -24,8 +99,8 @@ def user_signup(request):
                 user.save
                 login(request, user)
                 return redirect('/')
-            except:
-                error_message = 'Error creating account'
+            except IntegrityError:
+                error_message = 'Username already exists'
                 return render(request, 'signup.html', {'error_message':error_message})
         else:
              error_message = 'Password do not match'
